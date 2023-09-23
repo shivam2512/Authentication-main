084a31b9057e624490f1fc9bb7e38140ebd42866
shivam2512 <shivamshinde786@gmail.com>
2023-09-23 04:51:26 -0700

    Auto logout

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const AuthContext = React.createContext({
   token: '',
@@ -8,19 +8,48 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-    const initialToken=localStorage.getItem('token');
+  const initialToken = localStorage.getItem('token');
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
+
+  // Set an auto logout timer for 5 minutes (300,000 milliseconds)
+  const tokenExpirationTime = 300000; // 5 minutes in milliseconds
+  const logoutTimerRef = useRef();
 
   const loginHandler = (token) => {
     setToken(token);
     localStorage.setItem('token', token);
+
+    // Set an auto logout timer
+    logoutTimerRef.current = setTimeout(() => {
+      logoutHandler();
+      alert('You have been logged out due to inactivity. Please log in again.');
+    }, tokenExpirationTime);
   };
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+
+    // Clear the auto logout timer if the user manually logs out
+    if (logoutTimerRef.current) {
+      clearTimeout(logoutTimerRef.current);
+    }
   };
+
+  // Set up an effect to handle auto logout on component mount
+  useEffect(() => {
+    if (userIsLoggedIn) {
+      // Calculate the remaining time before token expiration
+      const remainingTime = tokenExpirationTime - (Date.now() - new Date(localStorage.getItem('tokenTimestamp')));
+
+      // Set an auto logout timer based on remaining time
+      logoutTimerRef.current = setTimeout(() => {
+        logoutHandler();
+        alert('You have been logged out due to inactivity. Please log in again.');
+      }, remainingTime);
+    }
+  }, [userIsLoggedIn]);
 
   const contextValue = {
     token: token,
