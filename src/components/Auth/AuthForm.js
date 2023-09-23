b5545b0aeecd9e4e533cf8c0048aaf4991eae564
shivam2512 <shivamshinde786@gmail.com>
2023-09-23 00:04:28 -0700

    Login Implementation

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -6,55 +6,61 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
   const submitHandler = (event) => {
-    event.preventDefault(); // Fix typo
+    event.preventDefault();
 
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true);
+    let url;
+
     if (isLogin) {
-      // Handle login
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBf_JF2M7GAjef9MD923pZ7gkzntUw4_hE'; // Replace with your API key
     } else {
-      fetch(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBf_JF2M7GAjef9MD923pZ7gkzntUw4_hE', // Replace with your API key
-        {
-          method: 'POST', // Fix typo
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-        .then((res) => {
-          setIsLoading(false);
-          if (res.ok) {
-            // Handle successful response here
-          } else {
-            return res.json().then((data) => {
-              let errorMessage='Authentication Failed !';
-              if(data && data.error && data.error.message)
-              {
-                errorMessage= data.error.message;
-              }
-              alert(errorMessage);
-              console.log(data);
-            });
-          }
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBf_JF2M7GAjef9MD923pZ7gkzntUw4_hE'; // Replace with your API key
     }
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Authentication Failed!';
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            setErrorMessage(errorMessage);
+            console.log(data);
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        // Handle successful login or signup here
+      })
+      .catch((error) => {
+        alert(errorMessage || error.message || 'An error occurred'); // Display the error message or a generic message
+      });
   };
 
   return (
@@ -80,6 +86,7 @@ const AuthForm = () => {
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
+       {/* {errorMessage && <p className={classes.error}>{errorMessage}</p>} */}
       </form>
     </section>
   );
